46fdc8d7b22f178d9921b06c24ffd911a6ed2c01
wing-kai <wing-kai@users.noreply.github.com>
2025-10-29 08:31:17 +0000

    deploy: fc4588dc2cc87e16c33ad585d211563bc54079d7

--- src/i18n/types.d.ts
+++
@@ -1,8 +0,0 @@
-// src/i18n/types.d.ts
-import 'vue-i18n';
-
-declare module 'vue-i18n' {
-  export interface DefineLocaleMessage {
-    // ... 其他翻译键
-  }
-}
--- uno.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'unocss'
-import presetUno from '@unocss/preset-uno'
-
-export default defineConfig({
-  presets: [
-    presetUno()
-  ],
-})
